d66d36c884ac524c6ef7d20c386375c135b10959
emalindahk <emalindahkimari@gmail.com>
2023-08-15 22:10:18 +0300

    Add navbar, clerk for auth, protected pages/routes with clerk and middleware

---
+++ components/Sidebar.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+import { cn } from "@/lib/utils";
+import { Code, ImageIcon, LayoutDashboard, MessageSquare, Music, Settings, VideoIcon } from "lucide-react";
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/conversation",
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/image-generation",
+    color: "text-pink-500",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/video",
+    color: "text-orange-700",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/music",
+    color: "text-emerald-500",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    href: "/code",
+    color: "text-green-700",
+  },
+  {
+    label: "Settings",
+    icon: Settings,
+    href: "/settings",
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-10">
+          <div className="md:hidden relative w-20 h-20">
+            <Image fill alt="Logo" src="/icon.png" />
+          </div>
+          <div className="hidden md:flex relative w-40 h-20 -ml-8 -mt-4">
+            <Image fill alt="Logo" src="/logo.svg" />
+          </div>
+        </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link href={route.href} key={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-5", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
