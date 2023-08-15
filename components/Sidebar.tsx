a5c2188f79d991ff8a9a8d0fa6b503f80915f384
emalindahk <emalindahkimari@gmail.com>
2023-08-15 22:52:37 +0300

    Add dashboard page

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 import { cn } from "@/lib/utils";
@@ -52,6 +53,8 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -62,7 +65,10 @@ const Sidebar = () => {
         </Link>
         <div className="space-y-1">
           {routes.map((route) => (
-            <Link href={route.href} key={route.href} className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition">
+            <Link href={route.href} 
+            key={route.href} 
+            className={cn("text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+            pathname === route.href ? "text-white bg-white/10" : "text-zinc-400")}>
               <div className="flex items-center flex-1">
                 <route.icon className={cn("h-5 w-5 mr-5", route.color)} />
                 {route.label}
