861738330db8a5debd0a0ebb1012b05d1dc76dce
emalindahk <emalindahkimari@gmail.com>
2024-01-12 11:35:38 +0300

    remove navbar and sidebar

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -7,11 +7,11 @@ const Dashboardlayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        <Sidebar />
+        {/* <Sidebar /> */}
         <Loader/>
       </div>
       <main className="md:pl-72">
-        <Navbar/>
+        {/* <Navbar/> */}
         {children}
       </main>
     </div>
