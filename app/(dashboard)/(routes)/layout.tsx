7393a0fab1e7e93e0f094c350a09f2c9b47b4311
emalindahk <emalindahkimari@gmail.com>
2024-01-12 11:42:39 +0300

    Versioning untracked files

--- app/(dashboard)/(routes)/layout.tsx
+++ app/(dashboard)/(routes)/layout.tsx
@@ -1,15 +1,15 @@
-import Loader from "@/components/loader";
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 import React from "react";
 
 const Dashboardlayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full relative">
       <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-        {/* <Sidebar /> */}
-        <Loader/>
+        <Sidebar />
       </div>
       <main className="md:pl-72">
-        {/* <Navbar/> */}
+        <Navbar/>
         {children}
       </main>
     </div>
